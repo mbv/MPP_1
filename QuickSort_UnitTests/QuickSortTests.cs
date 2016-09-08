34f5f67c610e0f8922e27013fc7a60e06543ab08
mbv <terehovk@ya.ru>
2016-09-08 18:00:55 +0300

    Add blank tests

---
+++ QuickSort_UnitTests/QuickSortTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QuickSort_UnitTests
+{
+    [TestClass]
+    public class QuickSortTests
+    {
+        [TestMethod]
+        public void WhenArrayIsSorted()
+        {
+        }
+
+        [TestMethod]
+        public void WhenArrayIsEmpty()
+        {
+        }
+
+        [TestMethod]
+        public void WhenArrayContainsNullValue()
+        {
+        }
+
+        [TestMethod]
+        public void WhenArrayIsNullValue()
+        {
+        }
+
+        [TestMethod]
+        public void WhenArrayIsReversedSorted()
+        {
+        }
+
+        [TestMethod]
+        public void WhenArrayContainsOnlyEqualsValues()
+        {
+        }
+
+        [TestMethod]
+        public void WhenArrayContainsOneElement()
+        {
+        }
+    }
+}
