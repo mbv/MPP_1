39c850d2619fa40ead5da33e2a54c3673215d9f0
mbv <terehovk@ya.ru>
2016-09-15 00:11:42 +0300

    Implementation QuickSort

--- QuickSort/QuickSort.cs
+++ QuickSort/QuickSort.cs
@@ -10,7 +10,37 @@ namespace QuickSort
     {
         public void Sort(T[] array, IComparer<T> comparer)
         {
-            throw new NotImplementedException();
+            if (array != null && array.Length > 1 && comparer != null)
+                SortArray(array, comparer, 0, array.Length - 1);
+        }
+
+        int partition(T[] array, IComparer<T> comparer, int left, int right)
+        {
+            int i = left;
+            for (int j = left; j <= right; j++)
+            {
+                if (comparer.Compare(array[j], array[right]) <= 0)
+                {
+                    Swap(array, i, j);
+                    i++;
+                }
+            }
+            return i - 1;
+        }
+
+        void SortArray(T[] array, IComparer<T> comparer, int left, int right)
+        {
+            if (left >= right) return;
+            int c = partition(array, comparer, left, right);
+            SortArray(array, comparer, left, c - 1);
+            SortArray(array, comparer, c + 1, right);
+        }
+
+        private static void Swap<T>(T[] array, int left, int right)
+        {
+            var temp = array[right];
+            array[right] = array[left];
+            array[left] = temp;
         }
     }
 }
