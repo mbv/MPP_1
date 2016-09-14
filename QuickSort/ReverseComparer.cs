fa98b5f0c582542f40ac36e31638e665c967eb24
mbv <terehovk@ya.ru>
2016-09-14 23:37:35 +0300

    Add ReverseComparer and add Tests

---
+++ QuickSort/ReverseComparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public sealed class ReverseComparer<T> : IComparer<T>
+    {
+        public static readonly ReverseComparer<T> Default = new ReverseComparer<T>(Comparer<T>.Default);
+
+        public static ReverseComparer<T> Reverse(IComparer<T> comparer)
+        {
+            return new ReverseComparer<T>(comparer);
+        }
+
+        private readonly IComparer<T> comparer = Default;
+
+        private ReverseComparer(IComparer<T> comparer)
+        {
+            this.comparer = comparer;
+        }
+
+        public int Compare(T x, T y)
+        {
+            return comparer.Compare(y, x);
+        }
+    }
+}
---
+++ QuickSort_UnitTests/ReverseComparerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickSort;
+
+namespace QuickSort_UnitTests
+{
+    [TestClass]
+    public class ReverseComparerTests
+    {
+        [TestMethod]
+        public void TestGreater()
+        {
+            IComparer<int> comparer = ReverseComparer<int>.Default;
+            Assert.IsTrue(comparer.Compare(2, 1) < 0);
+        }
+
+        [TestMethod]
+        public void TestLower()
+        {
+            IComparer<int> comparer = ReverseComparer<int>.Default;
+            Assert.IsTrue(comparer.Compare(1, 2) > 0);
+        }
+
+        [TestMethod]
+        public void TestEquals()
+        {
+            IComparer<int> comparer = ReverseComparer<int>.Default;
+            Assert.IsTrue(comparer.Compare(1, 1) == 0);
+        }
+    }
+}
